923042b4b1d74f9887958282ea32f14fa2e06f02
HeHeMan1234 <113253@cabrillopointacademy.org>
2026-03-12 01:32:24 -0700

    Create a new userscript for Veck utility modules.

---
+++ veck-utility-client.user.js
@@ -0,0 +1,38 @@
+// ==UserScript==
+// @name         Veck Utility Client
+// @namespace    http://your.namespace.here/
+// @version      1.0
+// @description  A collection of utilities for veck.io including FPS boost, nametag enlarger, and custom crosshairs.
+// @author       HeHeMan1234
+// @match        *://veck.io/*
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    // FPS Boost Module
+    const fpsBoost = () => {
+        // Code for FPS boost
+    };
+
+    // Nametag Enlarger Module
+    const nametagEnlarger = () => {
+        // Code for nametag enlarger
+    };
+
+    // Custom Crosshairs Module
+    const customCrosshairs = (theme) => {
+        // Code for custom crosshairs
+        if (theme === 'rainbow') {
+            // Rainbow theme code
+        } else if (theme === 'black') {
+            // Black theme code
+        }
+    };
+
+    // Initialize Modules
+    fpsBoost();
+    nametagEnlarger();
+    customCrosshairs('rainbow'); // Change to 'black' as needed
+})();
